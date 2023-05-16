c90ca7022f1bac2621b719595213eca8ebadf1a5
vosid <temirboyev0823@gmail.com>
2023-05-16 09:27:28 +0500

    removed all remove buttons

--- src/pages/A571.jsx
+++ src/pages/A571.jsx
@@ -12,9 +12,6 @@ function A571() {
               href="http://ftp.hikvision.ru/01.%20Firmware/01.%20Hikvision/01.%20IP%20Camera/01.%202%20series%20-%202XXX/E8%20platform%20%282CD2X23G0E%28B%29%29/5.7.1/IPCE_E8_EN_STD_5.7.1_211102.zip"
               className="button_div2"
             >
-              <i id="clear_" className="material-icons">
-                clear
-              </i>
               <h5>IPCE_E8_EN_STD_ 5.7.1_211102.zip</h5>
             </a>
 
--- src/pages/A5712.jsx
+++ src/pages/A5712.jsx
@@ -14,9 +14,6 @@ function A5712() {
               }
               className="button_div2"
             >
-              <i id="clear_" className="material-icons">
-                clear
-              </i>
               <h5>IPCE_E8_EN_STD_ 5.7.12_220819.zip dae</h5>
             </a>
             <a
@@ -24,9 +21,6 @@ function A5712() {
               href="http://ftp.hikvision.ru/01.%20Firmware/01.%20Hikvision/01.%20IP%20Camera/01.%202%20series%20-%202XXX/E8%20platform%20%282CD2X23G0E%28B%29%29/5.7.12/IPC_E8-V5.7.12_SP%20Release%20Note--EN.pdf"
               className="button_div2"
             >
-              <i id="clear_" className="material-icons">
-                clear
-              </i>
               <h5>IPC_E8-V5.7.12_SP Release Note--EN.pdf</h5>
             </a>
           </div>
