6c77178ab735e3ff10dd94b467520f0ce322b1b7
ShodiyevF <shodiyevf@yandex.ru>
2023-06-07 14:36:23 +0500

    fix bugs

--- src/pages/A1080Lite.jsx
+++ src/pages/A1080Lite.jsx
@@ -9,25 +9,23 @@ function A1080Lite() {
                 <div className='home_div2'>
                     <div className='button_div'>
                         <a target='_blank' href='http://ftp.hikvision.ru/03.%20Documents/01.%20How%20to%20documents/1080Lite/1080.jpg' className='button_div2'>
-                            <i id='clear_' className="material-icons">clear</i>
+                            
                             <h5>1080.jpg </h5>
                         </a>
                         <a target='_blank' href='http://ftp.hikvision.ru/03.%20Documents/01.%20How%20to%20documents/1080Lite/1080lite.jpg' className='button_div2'>
-                            <i id='clear_' className="material-icons">clear</i>
+                            
                             <h5>1080lite.jpg</h5>
                         </a>
                         <a target='_blank' href='http://ftp.hikvision.ru/03.%20Documents/01.%20How%20to%20documents/1080Lite/comp.jpg' className='button_div2'>
-                            <i id='clear_' className="material-icons">clear</i>
+                            
                             <h5>comp.jpg</h5>
                         </a>
                         <a target='_blank' href='http://ftp.hikvision.ru/03.%20Documents/01.%20How%20to%20documents/1080Lite/compzoom.jpg' className='button_div2'>
-                            <i id='clear_' className="material-icons">clear</i>
+                            
                             <h5>compzoom.jpg</h5>
                         </a>
                     </div>
-                    <div>
-                        <img src={img} alt="" />
-                    </div>
+                    
                 </div>
             </div>
         </div>
